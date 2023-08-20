1c2c71f5d615f047da56e2d8725b818d735edac2
EddWlad <tidsec_inf@outlook.com>
2023-08-20 13:58:37 -0500

    Mostrar el resultado en el componente lblValorIVA

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -42,6 +42,8 @@ calcularValorTotal = function(){
     mostrarTexto("lblDescuento",valorDescuento);
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
 	//   El IVA debe calcularse sobre el valor del subtotal menos el descuento
+    let valor = valorSubtotal - valorDescuento;
+    valorIVA = calcularIva(valor);
     //9. Mostrar el resultado en el componente lblValorIVA    
     /*
             Caso de prueba: 
@@ -52,6 +54,7 @@ calcularValorTotal = function(){
                 IVA esperado: 5.832
             Si el caso de prueba es exitoso, hacer un commit
         */
+    mostrarTexto("lblValorIVA",valorIVA);
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
     //11. Mostrar el resultado en el componente lblTotal
      /*
