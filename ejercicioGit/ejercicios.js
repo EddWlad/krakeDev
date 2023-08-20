c9a7583e462ad6bb94dc7d5b3b365a1289496427
EddWlad <tidsec_inf@outlook.com>
2023-08-20 09:53:40 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicios.js
+++ ejercicioGit/ejercicios.js
@@ -1,4 +1,8 @@
 saludar= function() 
 {
+    let cmpimagenLucha;
+    cmpimagenLucha = document.getElementById("imgLucha");
+    cmpimagenLucha.src="./img/tanjiro1.gif"
     console.log("Ingresa a saludar");
+
 }
