82bf29b42c27cd93dc0bb1817c39f29a1166cf97
EddWlad <tidsec_inf@outlook.com>
2023-08-20 16:31:14 -0500

    visualizacion de mensaje en pantalla con cambiarTexto en Dados

---
+++ DadosArchivos/DadosArchivos/dados.js
@@ -0,0 +1,25 @@
+jugar = function()
+{
+    let aleatorio = lazarDado();
+    cambiarTexto("lblNumero", aleatorio);
+    if(aleatorio>3)
+    {
+        cambiarTexto("lblMensaje", "ES MAYOR A 3 GANASTE");
+    }
+    else
+    {
+        cambiarTexto("lblMensaje", "ES MENOR A 3 GANASTE PERDISTE ESTAS DE MALAS");
+    }
+}
+
+//Crear una funcion llamada lanzarDado
+//no recibe parametros
+//Retorna un numero aleatorio entre 1 y 6
+
+lazarDado = function()
+{
+    let valorDado;
+    let aleatorio = Math.random();
+    valorDado = parseInt(aleatorio*6)+1;
+    return valorDado;
+}
