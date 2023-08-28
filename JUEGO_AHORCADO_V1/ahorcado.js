11199d7c67f764baa9af3a4f6e2f3de3eaff805b
EddWlad <tidsec_inf@outlook.com>
2023-08-28 13:48:41 -0500

    implementacion de funcion ingresarLetra, prueba de juego

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -20,6 +20,7 @@ esMayuscula = function(caracter)
 guardarPalabra = function()
 {
     let palabra = recuperarTexto("txtSecreta");
+    let letra = "";
     if((palabra.length != 5))
     {
         alert("Ingrese la palabra con 5 letras y mayusculas");     
@@ -28,7 +29,7 @@ guardarPalabra = function()
     {
         for(i=0;i<palabra.length;i++)
         {
-            let letra = palabra.charAt(i);
+            letra = palabra.charAt(i);
             if (esMayuscula(letra))
             {
                 palabraSecreta+=letra;
@@ -48,19 +49,19 @@ mostrarLetra = function(letra,posicion)
 {
     switch(posicion)
     {
-        case "0":
+        case 0:
             mostrarTexto("div0",letra);
             break;
-        case "1":
+        case 1:
             mostrarTexto("div1",letra);
             break;
-        case "2":
+        case 2:
             mostrarTexto("div2",letra);
             break;
-        case "3":
+        case 3:
             mostrarTexto("div3",letra);
             break;
-        case "4":
+        case 4:
             mostrarTexto("div4",letra);
             break;
     }
@@ -68,14 +69,27 @@ mostrarLetra = function(letra,posicion)
 
 validar = function(letra)
 {
-    let letrasEncontradas;
+    let letrasEncontradas =0;
     for(i=0; i<palabraSecreta.length; i++)
     {
-        let caracter = palabra.charAt(i);
+        let caracter = palabraSecreta.charAt(i);
         if (caracter == letra)
         {
             mostrarLetra(caracter,i);
             letrasEncontradas += 1;
         }
+    }
+}
+
+ingresarLetra = function()
+{
+    let letra = recuperarTexto("txtLetra");
+    if(esMayuscula(letra))
+    {
+        validar(letra);
+    }
+    else
+    {
+        alert("SOLO SE ACEPTAN MAYUSCULAS");
     }
 }
